80b3e780be9f3e67af9fe72b0168585c8dea5ccf
Caroline Boyt <mboyt@g.clemson.edu>
2022-03-16 11:00:01 -0400

    work on auditions page and overall formatting

--- src/pages/auditions.js
+++ src/pages/auditions.js
@@ -1,13 +1,27 @@
 import * as React from "react"
-import './index.css'
+import './auditions.css'
+
+import picLogo from '../images/logo-with-pics.jpg'
 
 import Navbar from "../components/navbar"
 
 const Auditions = () => {
     return (
         <>
-            <Navbar />
-            <h1>Auditions</h1>
+        <div class="audition-navbar">
+            <Navbar/>
+        </div>
+        
+        <div class="audition-wrapper">
+            <div class="audition-feature">
+                <div className="audition-header">
+                    <h1>Join a Choir</h1>
+                </div>
+            </div>
+            <div class="audition-content">
+
+            </div>
+        </div>
         </>
     )
 }
