9e3dd38360c8c770070fad2665efb78d4732e655
Caroline Boyt <m.caroline.boyt@gmail.com>
2022-03-16 12:01:39 -0400

    auditions page

--- src/pages/auditions.js
+++ src/pages/auditions.js
@@ -1,9 +1,10 @@
 import * as React from "react"
 import './auditions.css'
 
-import picLogo from '../images/logo-with-pics.jpg'
-
 import Navbar from "../components/navbar"
+import CustomButton from '../components/custom-button'
+import Footer from '../components/footer'
+
 
 const Auditions = () => {
     return (
@@ -19,8 +20,27 @@ const Auditions = () => {
                 </div>
             </div>
             <div class="audition-content">
-
+                <div className="audition-left">
+                    <h2>Thank you for your interest in Clemson Choirs! Each of our choral ensembles are open to ALL students at Clemson University.</h2>
+                    <p>Auditions for CU Singers (MUSC 3700) and Cantorei (MUSC 3450) for the Fall 2022 Semester will be announced later this Spring.</p>
+                    <p>If you would like to be a part of Men's Choir (MUSC 3720) or Women’s Choir (MUSC 3710), no audition is necessary! Simply register for the class on iRoar.</p>
+                    <p>For more information on Tigeroar and TakeNote auditions contact Dr. David Conley.</p>
+                </div>
+                <div className="audition-right">
+                    <div class="side-content">
+                        <h3>Interest Form</h3>
+                        <hr class="solid"></hr>
+                        <p>If you would like more information about Clemson Choirs, please fill out this short form to receive a personal email from our director.</p>
+                        <div id="custom-button">
+                            <CustomButton label="Go to Form"/>
+                        </div>
+                    </div>
+                </div>
             </div>
+        </div>
+
+        <div className="foot">
+          <Footer/>
         </div>
         </>
     )
