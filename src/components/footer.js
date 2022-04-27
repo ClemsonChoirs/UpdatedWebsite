9c85df0e23756d60bc4fb91e32cb6a90e7013369
Caroline Boyt <m.caroline.boyt@gmail.com>
2022-04-27 11:27:33 -0400

    footer social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faFacebookSquare, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
-import instagram from '../images/instagram.png'
-import facebook from '../images/facebook.png'
-import youtube from '../images/youtube.png'
 import './stylesheets/components.css'
 
 const Footer = () => {
@@ -18,8 +15,8 @@ const Footer = () => {
                 </div>
                 <div className="socials">
                     <a href="https://www.instagram.com/clemsonchoirs/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
-                    <a href="https://www.youtube.com/c/ClemsonChoirsandVocalArts" target="_blank" rel="noreferrer"><img src={youtube} alt="youtube logo"/></a>
-                    <a href="https://www.facebook.com/clemsonchoirs" target="_blank" rel="noreferrer"><img src={facebook} alt="facebook logo"/></a>
+                    <a href="https://www.youtube.com/c/ClemsonChoirsandVocalArts" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faYoutube} /></a>
+                    <a href="https://www.facebook.com/clemsonchoirs" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faFacebookSquare} /></a>
                 </div>
                 <div className="dr-b-second">
                     <p>Dr. Anthony Bernarducci</p>
