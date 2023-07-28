9e0e6f629d99bdd54063fce08d9467057174fdfe
Caroline Boyt <caroline@arccosgolf.com>
2023-07-28 12:07:08 -0400

    takenote & tigeroar audition updates

--- src/pages/takenote.js
+++ src/pages/takenote.js
@@ -31,14 +31,23 @@ const Cantorei = () => {
                     <p>TakeNote rehearses on Tuesdays and Thursdays in Brooks 117, meeting from 5pm to 6:15pm during the Fall semester, and from 6:30pm to 7:45pm in the Spring. To enroll, register for MUSC 3420 (Women's Breakout Ensemble)</p>
                     <div className="ensemble-buttons">
                         <Link to="mailto:conley2@clemson.edu"><BigButton label="Email the Faculty Director"/></Link>
-                        <Link to="../auditions"><BigButton disabled label="Audition Information Coming Soon!"/></Link>
-
+                        <a href="https:/instagram.com/clemsontakenote" target="_blank"><BigButton label="@ClemsonTakeNote on Instagram"/></a>
                     </div>
-                    
                 </div>
                 <div className="ensemble-right">
                     <VideoPanel title={"Ain't No Other Man"} subtitle={""} video={"https://www.youtube.com/embed/uZUZe9EvGgg"} bodytext="As performed by Christina Aguilera, arranged by Kyle McTavish."/>
                 </div>
+            </div>
+            <div className="audition-info-wrapper">
+                <h2>Audition Information</h2>
+                <p>Auditions for the Fall 2023 semester will be held at 5pm on Thursday, August 24th in the Brooks Center for the Performing Arts, Room 117.</p>
+                <p className="audition-info">Auditions will include the following:</p>
+                <ol>
+                    <li>Questionnaire: Tell us about yourself!</li>
+                    <li>Range Test: Vocal exercises to test the upper and lower limits of your vocal range.</li>
+                    <li>Pitch Matching: Listen to a series of pitches, then match the pitches with your voice.</li>
+                    <li>Solo Performance: 1-2 minutes (a verse and chorus) of a song to showcase your voice.</li>
+                </ol>
             </div>
 
             <div className="ensemble-foot">
--- src/pages/tigerroar.js
+++ src/pages/tigerroar.js
@@ -31,14 +31,23 @@ const Cantorei = () => {
                     <p>TigerRoar rehearses on Tuesdays and Thursdays in Brooks 117, meeting from 5pm to 6:15pm during the Spring semester, and from 6:30pm to 7:45pm in the Fall. To enroll, register for MUSC 3410 (Men's Breakout Ensemble).</p>
                     <div className="ensemble-buttons">
                         <Link to="mailto:conley2@clemson.edu"><BigButton label="Email the Faculty Director"/></Link>
-                        <Link to="../auditions"><BigButton disabled label="Audition Information Coming Soon!"/></Link>
-                    </div>
-                    
+                        <a href="https:/instagram.com/tigeroarclemson" target="_blank"><BigButton label="@TIGEROARClemson on Instagram"/></a>
+                    </div> 
                 </div>
                 <div className="ensemble-right">
                     <VideoPanel title={"Uptown Girl"} subtitle={""} video={"https://www.youtube.com/embed/6yF4nFsg_fQ"} bodytext="As performed by Billy Joel, arranged by Jake Liguori."/>
-                    <VideoPanel title={"Paradise"} subtitle={""} video={"https://www.youtube.com/embed/yN_QywPXSAo"} bodytext="As performed by Coldplay, arranged by Thomas Catoe."/>
                 </div>
+            </div>
+            <div className="audition-info-wrapper">
+                <h2>Audition Information</h2>
+                <p>Auditions for the Fall 2023 semester will be held at 6:30pm on Thursday, August 24th in the Brooks Center for the Performing Arts, Room 117.</p>
+                <p className="audition-info">Auditions will include the following:</p>
+                <ol>
+                    <li>Questionnaire: Tell us about yourself!</li>
+                    <li>Range Test: Vocal exercises to test the upper and lower limits of your vocal range.</li>
+                    <li>Pitch Matching: Listen to a series of pitches, then match the pitches with your voice.</li>
+                    <li>Solo Performance: 1-2 minutes (a verse and chorus) of a song to showcase your voice.</li>
+                </ol>
             </div>
 
             <div className="ensemble-foot">
